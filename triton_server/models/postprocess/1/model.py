386ecb1f5c78403d6727f7264bf5af15175f4922
PitKoro <pit.koro@gmail.com>
2023-02-06 05:42:41 +0300

    конвертация id класса в label на стороне triton

--- triton_server/models/postprocess/1/model.py
+++ triton_server/models/postprocess/1/model.py
@@ -1,7 +1,21 @@
+import os
 import triton_python_backend_utils as pb_utils
+import numpy as np
 
 
 class TritonPythonModel:
+    
+    def __init__(self):
+        super().__init__()
+        self.labels = self.get_labels_from_file()
+
+
+    def get_labels_from_file(self, file_path=f"{os.path.dirname(os.path.realpath(__file__))}/labels.txt"):
+        with open(file_path, "r") as labels_file:
+            labels = labels_file.read().split('\n')
+        return labels
+
+
     def get_inputs(self, request):
         input_tensor_names = ['bboxes', 'classes', 'scores', 'shape']
         inputs = {
@@ -10,10 +24,15 @@ class TritonPythonModel:
         }
         return inputs
 
+
     def execute(self, requests):
         responses = []
         for request in requests:
             predictions = self.get_inputs(request)
+            predicted_labels = []
+            for i in predictions['classes']:
+                predicted_labels.append(self.labels[i])
+            predictions['classes'] = np.array(predicted_labels, dtype=object)
             out_tensors = []
             for name in ['bboxes', 'classes', 'scores']:
                 tensor = pb_utils.Tensor('post_' + name, predictions[name])
